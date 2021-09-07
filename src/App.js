2c5ec1ce26e938102da313d8d84e2a771228d208
Kolya Shevelinda <kolyashevelinda@gmail.com>
2021-09-07 17:11:38 +0300

    add component TodoList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import TodoList from "./Todo/TodoList";
+
 function App() {
-  return <div />
+  return (
+      <div className="wrapper">
+          <h1>React tutorial</h1>
+          
+          <TodoList/>
+      </div>
+  )
 }
 
 export default App;
